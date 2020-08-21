5eeb0306e3a5bf33e9ec97340604c89b51fc1517
adam.gatt <adam.gatt@penguin.lxd>
2020-08-21 11:55:58 +0930

    Improve database handling

--- backend/backend.py
+++ backend/backend.py
@@ -1,17 +1,23 @@
 import boto3
 from flask import Flask
+from typing import Optional
 import os
 
-def get_db_endpoint() -> str:
-    return os.getenv('DB_ENDPOINT', '')
+def is_local() -> bool:
+    return 'LOCAL_DB_ENDPOINT' in os.environ
+
+def get_db_endpoint() -> Optional[str]:
+    return os.getenv('LOCAL_DB_ENDPOINT', '')
 
 # For a Boto3 client.
-ddb = boto3.client('dynamodb', 
-    endpoint_url=get_db_endpoint(),
-    region_name='ap-southeast-2',
-    aws_access_key_id='anything',
-    aws_secret_access_key='anything'
-)
+ddb_keyargs = ({
+    'endpoint_url': get_db_endpoint(),
+    'region_name': 'ap-southeast-2',
+    'aws_access_key_id': 'anything',
+    'aws_secret_access_key': 'anything'
+} if is_local() else {
+    'region_name': 'ap-southeast-2',
+})
 
 app = Flask(__name__)
 
@@ -21,5 +27,6 @@ def hello():
 
 @app.route('/test/')
 def test_dynamodb():
+    ddb = boto3.client('dynamodb', **ddb_keyargs)
     response = ddb.list_tables()
     return {'message': response['TableNames']}
